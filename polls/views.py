4f9ed8cfb5253b274800d9780e25bae19fb1d959
tarmes <tarmes@github.com>
2017-05-10 22:16:18 -0400

    Namespacing URL names

--- polls/views.py
+++ polls/views.py
@@ -3,11 +3,7 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
